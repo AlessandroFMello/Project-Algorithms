a229d91b195013477b7c2e2d0fdc22c0c2316df2
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-07-27 14:41:49 -0300

    refactor: corrects linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     try:
-        counter = [pair for pair in permanence_period if int(target_time) in range(pair[0], pair[-1] + 1)]
+        counter = list()
+        for pair in permanence_period:
+            if int(target_time) in range(pair[0], pair[-1] + 1):
+                counter.append(pair)
 
         return len(counter)
     except TypeError:
