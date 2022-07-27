d71e46eb875c99e3f2b0c7c4a3ac5e968e016c00
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-07-27 14:23:40 -0300

    feat: Implements study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        counter = [pair for pair in permanence_period if int(target_time) in range(pair[0], pair[-1] + 1)]
+
+        return len(counter)
+    except TypeError:
+        return None
