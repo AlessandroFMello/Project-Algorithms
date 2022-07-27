ea5cc7d0be2c2ed8f5f30e52e8648943c3d57803
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-07-27 14:24:13 -0300

    feat: Implements is_palindrome_recursive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    try:
+        if word == '' or word[0] != word[-1]:
+            return False
+        elif len(word) == 1 or (len(word) == 2 and word[0] == word[-1]):
+            return True
+        else:
+            return is_palindrome_recursive(word[1:-1], low_index, high_index)
+    except ValueError:
+        return False
